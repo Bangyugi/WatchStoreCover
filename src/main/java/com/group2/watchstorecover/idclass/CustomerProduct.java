8c47dec1a6f56d0313d5ce1eabf40aa08708adad
Bangyugi <bangtranvan08@gmail.com>
2024-11-16 21:05:05 +0700

    complete basic setup

--- src/main/java/com/group2/watchstorecover/idclass/CustomerProduct.java
+++ src/main/java/com/group2/watchstorecover/idclass/CustomerProduct.java
@@ -2,11 +2,15 @@ package com.group2.watchstorecover.idclass;
 
 import com.group2.watchstorecover.entity.Customer;
 import com.group2.watchstorecover.entity.Product;
-import lombok.AccessLevel;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 import java.io.Serializable;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @FieldDefaults  (level = AccessLevel.PRIVATE)
 public class CustomerProduct implements Serializable {
     Customer customer;
